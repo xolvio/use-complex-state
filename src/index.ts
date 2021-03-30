e1b653a6571dfb9cd48eb37f8d5bc66e3e285141
lgandecki <lgandecki@css.edu>
2021-03-30 11:30:40 +0200

    initial version

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { useReducer } from "react";
+import {
+  createSlice,
+  CreateSliceOptions,
+  SliceCaseReducers,
+} from "@reduxjs/toolkit";
+
+type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+
+export const useComplexState = <
+  State,
+  CaseReducers extends SliceCaseReducers<State>,
+  Name extends string = string
+  >(
+  options: Optional<CreateSliceOptions<State, CaseReducers, Name>, "name">
+) => {
+  const slice = createSlice({ name: "__name", ...options });
+  const [state, dispatch] = useReducer(slice.reducer, options.initialState);
+  Object.entries(slice.actions).forEach(
+    ([name, action]) => {
+      // @ts-ignore
+      slice.actions[name] = (...args) => {
+        // @ts-ignore
+        dispatch(action(...args));
+      };
+    }
+  );
+  return [state, slice.actions, dispatch] as const;
+};
